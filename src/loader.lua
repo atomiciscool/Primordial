15ec059bd32ce80cba18732130e76b1dd3ce331f
atomiciscool <146490689+atomiciscool@users.noreply.github.com>
2024-05-09 15:35:00 +0200

    Update loader.lua

--- src/loader.lua
+++ src/loader.lua
@@ -2,7 +2,7 @@ if (not game:IsLoaded()) then game.Loaded:Wait() end
 
 local gameid = game.PlaceId
 local HttpService = game:GetService("HttpService")
-local PlacesData = game:HttpGet("https://raw.githubusercontent.com/Bovanlaarhoven/Primordial/main/src/places.json")
+local PlacesData = game:HttpGet("https://raw.githubusercontent.com/atomiciscool/Primordial/main/src/places.json")
 local Places = HttpService:JSONDecode(PlacesData)
 local getconnections = debug.getconnections
 local DisableConnection = {ScriptContext, LogService}
@@ -38,7 +38,7 @@ if not isfolder("Primordial") then
 end
 
 if supported then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Bovanlaarhoven/Primordial/main/src/main.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/atomiciscool/Primordial/main/src/main.lua"))()
 else
     game:GetService("Players").LocalPlayer:Kick(gameid .. " is not supported by Primordial.")
 end
